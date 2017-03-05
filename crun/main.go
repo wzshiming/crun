812d1be281e3986b1bebc29434afb38e15a15085
wzshiming <wzshiming@foxmail.com>
2017-03-05 16:26:29 +0800

    整理

--- crun/main.go
+++ crun/main.go
@@ -20,9 +20,9 @@ func main() {
 		s := os.Args[0]
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", s)
 		fmt.Fprintf(os.Stderr, "\t   %s <regexp>\n", s)
-		fmt.Fprintf(os.Stderr, "\tor %s \\d{3}\n", s)
-		fmt.Fprintf(os.Stderr, "\tor %s [0-9a-z]{2}\n", s)
-		fmt.Fprintf(os.Stderr, "\tor %s \"(root|admin)\" [0-9]{1}\n", s)
+		fmt.Fprintf(os.Stderr, "\tor %s \"\\d{3}\"\n", s)
+		fmt.Fprintf(os.Stderr, "\tor %s \"[0-9a-z]{2}\"\n", s)
+		fmt.Fprintf(os.Stderr, "\tor %s \"(root|admin) [0-9]{1}\"\n", s)
 
 		return
 	}
