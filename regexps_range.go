3b56bbad6279c086dbeea2544d912b044a2367b4
wzshiming <wzshiming@foxmail.com>
2018-08-30 16:06:24 +0800

    fixes range

--- regexps_range.go
+++ regexps_range.go
@@ -25,11 +25,10 @@ func ranges(r Regexps, buf []rune, off int, f func([]rune) bool) bool {
 	case OpLiteral: // matches Runes sequence
 		return ff(reg.Rune)
 	case OpRepeat: // matches Sub[0] at least Min times, at most Max (Max == -1 is no limit)
-		ru := reg.Rune
-		if len(reg.Sub) != 0 {
-			ru = reg.Sub[0][0].Rune
+		if len(reg.Sub) == 0 {
+			return rangeRepeatPossibilities(reg.Rune, reg.Min, reg.Max, ff)
 		}
-		return rangePossibilities(ru, reg.Min, reg.Max, ff)
+		return rangePossibilities(reg.Sub, reg.Min, reg.Max, ff)
 	case OpAlternate: // matches alternation of Subs
 		for _, v := range reg.Sub {
 			if !ranges(append(v, r[1:]...), buf, off, f) {
@@ -43,7 +42,37 @@ func ranges(r Regexps, buf []rune, off int, f func([]rune) bool) bool {
 	}
 }
 
-func rangePossibilitie(runes []rune, buf []rune, ff func(r []rune) bool) bool {
+func rangePossibilitie(regs []Regexps, buf []rune, ff func(r []rune) bool) bool {
+	if len(regs) == 0 {
+		return ff(buf)
+	}
+	return ranges(regs[0], buf, len(buf), func(r []rune) bool {
+		return rangePossibilitie(regs[1:], r, ff)
+	})
+}
+
+func rangePossibilitie2(regs []Regexps, buf []rune, size int, ff func(r []rune) bool) bool {
+	if size == 0 {
+		return ff(buf)
+	}
+
+	return rangePossibilitie(regs, buf, func(r []rune) bool {
+		return rangePossibilitie2(regs, r, size-1, ff)
+	})
+}
+
+// rangePossibilities range all possibilities.
+func rangePossibilities(regs []Regexps, min int, max int, ff func(r []rune) bool) bool {
+	for i := min; i <= max; i++ {
+		buf := []rune{}
+		if !rangePossibilitie2(regs, buf, i, ff) {
+			return false
+		}
+	}
+	return true
+}
+
+func rangeRepeatPossibilitie(runes []rune, buf []rune, ff func(r []rune) bool) bool {
 	if len(buf) == cap(buf) {
 		return ff(buf)
 	}
@@ -51,7 +80,7 @@ func rangePossibilitie(runes []rune, buf []rune, ff func(r []rune) bool) bool {
 	for i := 0; i < len(runes); i += 2 {
 		for j := runes[i]; j <= runes[i+1]; j++ {
 			buf[len(buf)-1] = j
-			if !rangePossibilitie(runes, buf, ff) {
+			if !rangeRepeatPossibilitie(runes, buf, ff) {
 				return false
 			}
 		}
@@ -59,14 +88,14 @@ func rangePossibilitie(runes []rune, buf []rune, ff func(r []rune) bool) bool {
 	return true
 }
 
-// rangePossibilities range all possibilities.
-func rangePossibilities(runes []rune, min int, max int, ff func(r []rune) bool) bool {
+// rangeRepeatPossibilities range all possibilities.
+func rangeRepeatPossibilities(runes []rune, min int, max int, ff func(r []rune) bool) bool {
 	if len(runes) == 1 {
 		runes = append(runes, runes[0])
 	}
 	for i := min; i <= max; i++ {
 		buf := make([]rune, 0, i)
-		if !rangePossibilitie(runes, buf, ff) {
+		if !rangeRepeatPossibilitie(runes, buf, ff) {
 			return false
 		}
 	}
